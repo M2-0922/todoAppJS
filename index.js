c49f1230461fc2c74b135343ae2faea044c950f9
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-01-26 10:24:08 -0800

    first commit

--- index.js
+++ index.js
@@ -1,53 +1,99 @@
 window.addEventListener("load", () => {
     let inputData = document.getElementById("todo-input");
     let addButton = document.getElementById("todo-add");
-    let todoList = document.getElementById("todos-list");
+    let todoList = document.getElementById("todo-list");
     let searchInput = document.getElementById("todo-search");
+
+    inputData.addEventListener("keyup", (event) => {
+        console.log(event);
+        if (event.key === "Enter") {
+            addButton.click();
+        }
+    })
 
     addButton.addEventListener("click", () => {
         let newTodo = document.createElement("li");
-        newTodo.innerHTML = inputData.value;
         let deleteButton = document.createElement("button");
+        let editButton = document.createElement("button");
+        let isCompleted = document.createElement("input");
 
-        deleteButton.innerText = "Delete"
-        // deleteButton.classList.add("delete-button");
- 
-        todoList.appendChild(deleteButton);
+        // Checkbox
+        isCompleted.type = "checkbox";;
+        isCompleted.setAttribute("id", "todo-checkbox");
+
+        // delete button
+        deleteButton.innerText = "Delete";
+        deleteButton.setAttribute("id", "todo-delete");
+
+        // edit button
+        editButton.innerText = "Edit";
+        editButton.setAttribute("id", "todo-edit");
+
+        // newTodo
+        newTodo.innerHTML = inputData.value;
+        newTodo.appendChild(editButton);
+        newTodo.appendChild(deleteButton);
+        newTodo.insertBefore(isCompleted, newTodo.childNodes[0]);
+
+        // todoList.appendChild(deleteButton);
         todoList.appendChild(newTodo);
 
         inputData.value = "";
 
-        // delete button
-        // deleteButton.addEventListener("click", () => {
-        //     alert("do you realy want to delete ?");
-        // })
+        deleteButton.addEventListener("click", () => {
+            let isAccept = confirm("Are you sure you wanna delete " + newTodo.innerText + "?");
 
-        // also add checkbox to your todo while creating.
-        
+            if (isAccept) {
+                newTodo.remove();
+                deleteButton.remove();
+            } else {
+                alert("Deleting failed.");
+            }
+        })
 
+        editButton.addEventListener("click", () => {
+            let editInput = document.createElement("input");
+            let saveButton = document.createElement("button");
+
+            // saveButton
+            saveButton.innerText = "Save";
+            saveButton.setAttribute("id", "todo-save");
+
+            // editInput
+            editInput.setAttribute("id", "edit-input");
+
+            newTodo.innerHTML = "";
+            newTodo.appendChild(editInput);
+            newTodo.appendChild(saveButton);
+
+            saveButton.addEventListener("click", () => {
+                newTodo.innerHTML = "";
+                newTodo.appendChild(isCompleted);
+
+                newTodo.appendChild(document.createTextNode(editInput.value));
+                newTodo.appendChild(deleteButton);
+                newTodo.appendChild(editButton);
+            })
+
+            editInput.addEventListener("keyup", (e) => {
+                if (e.key === "Enter") {
+                    saveButton.click();
+                }
+            })
+        })
     })
-    
 
-    searchInput.addEventListener("keyup", () => {
-
+    searchInput.addEventListener("keyup", (event) => {
         let searchData = searchInput.value;
-
         let todos = Array.from(todoList.children);
 
-        // todos.forEach((kubilay) => {
-        //     if(kubilay.innerText.includes(searchData)){
-        //         kubilay.style.display = "block";
-        //     }else{
-        //         kubilay.style.display = "none";
-        //     }
-        // })
-
-        for(let i = 0; i < todos.length; i++){
-            if(todos[i].innerText.includes(searchData)){
-                todos[i].style.display = "block";
-            }else{
-                todos[i].style.display = "none";
+        todos.forEach((todo) => {
+            if (todo.innerText.includes(searchData)) {
+                todo.style.display = "block";
+            } else {
+                todo.style.display = "none";
             }
-        }
+        })
+
     })
 })
