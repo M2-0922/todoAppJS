188b7e9e4f38723140b0c1a5e530e3ade3cacc48
Aya <pyon220@gmail.com>
2023-01-25 17:55:21 -0800

    resolving mistake in commiting to master

--- index.js
+++ index.js
@@ -1,61 +1,53 @@
 window.addEventListener("load", () => {
-  let inputData = document.getElementById("todo-input");
-  let addButton = document.getElementById("todo-add");
-  let todoList = document.getElementById("todos-list");
-  let searchInput = document.getElementById("todo-search");
+    let inputData = document.getElementById("todo-input");
+    let addButton = document.getElementById("todo-add");
+    let todoList = document.getElementById("todos-list");
+    let searchInput = document.getElementById("todo-search");
 
-  addButton.addEventListener("click", () => {
-    let newTodo = document.createElement("li");
-    // newTodo.classList.add("li-list");
-    newTodo.innerHTML = inputData.value;
-    if (inputData.value) {
-      let deleteButton = document.createElement("button");
-      var checkbox = document.createElement("INPUT");
-      checkbox.setAttribute("type", "checkbox");
-      checkbox.classList.add("check-box");
-      //   newTodo.innerHTML = inputData.value;
+    addButton.addEventListener("click", () => {
+        let newTodo = document.createElement("li");
+        newTodo.innerHTML = inputData.value;
+        let deleteButton = document.createElement("button");
 
-      deleteButton.innerText = "Delete";
-      deleteButton.classList.add("delete-button");
+        deleteButton.innerText = "Delete"
+        // deleteButton.classList.add("delete-button");
+ 
+        todoList.appendChild(deleteButton);
+        todoList.appendChild(newTodo);
 
-      newTodo.appendChild(checkbox);
-      todoList.appendChild(newTodo);
-      newTodo.appendChild(deleteButton);
-      //   newTodo.innerHTML = inputData.value;
+        inputData.value = "";
 
-      inputData.value = "";
+        // delete button
+        // deleteButton.addEventListener("click", () => {
+        //     alert("do you realy want to delete ?");
+        // })
 
-      delete button;
-      deleteButton.addEventListener("click", () => {
-        alert("do you really want to delete ?");
-        newTodo.remove();
-        deleteButton.remove();
-        checkbox.remove();
-      });
-    }
+        // also add checkbox to your todo while creating.
+        
 
-    // also add checkbox to your todo while creating.
-  });
+    })
+    
 
-  searchInput.addEventListener("keyup", () => {
-    let searchData = searchInput.value;
+    searchInput.addEventListener("keyup", () => {
 
-    let todos = Array.from(todoList.children);
+        let searchData = searchInput.value;
 
-    todos.forEach((todo) => {
-      if (todo.innerText.includes(searchData)) {
-        todo.style.display = "block";
-      } else {
-        todo.style.display = "none";
-      }
-    });
+        let todos = Array.from(todoList.children);
 
-    // for (let i = 0; i < todos.length; i++) {
-    //   if (todos[i].innerText.includes(searchData)) {
-    //     todos[i].style.display = "block";
-    //   } else {
-    //     todos[i].style.display = "none";
-    //   }
-    // }
-  });
-});
+        // todos.forEach((kubilay) => {
+        //     if(kubilay.innerText.includes(searchData)){
+        //         kubilay.style.display = "block";
+        //     }else{
+        //         kubilay.style.display = "none";
+        //     }
+        // })
+
+        for(let i = 0; i < todos.length; i++){
+            if(todos[i].innerText.includes(searchData)){
+                todos[i].style.display = "block";
+            }else{
+                todos[i].style.display = "none";
+            }
+        }
+    })
+})
