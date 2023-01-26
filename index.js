4a0b6adbdc13113490f0ff088d3b2f31ec116e65
Yuya Minamide <dettu1003@gmail.com>
2023-01-25 17:17:50 -0800

    added checkbox and delete button

--- index.js
+++ index.js
@@ -1,53 +1,79 @@
 window.addEventListener("load", () => {
-    let inputData = document.getElementById("todo-input");
-    let addButton = document.getElementById("todo-add");
-    let todoList = document.getElementById("todos-list");
-    let searchInput = document.getElementById("todo-search");
+	let inputData = document.getElementById("todo-input");
+	let addButton = document.getElementById("todo-add");
+	let todoList = document.getElementById("todos-list");
+	let searchInput = document.getElementById("todo-search");
 
-    addButton.addEventListener("click", () => {
-        let newTodo = document.createElement("li");
-        newTodo.innerHTML = inputData.value;
-        let deleteButton = document.createElement("button");
+	addButton.addEventListener("click", () => {
+		let newTodo = document.createElement("li");
+		let checkbox = document.createElement("input");
+		let deleteButton = document.createElement("button");
 
-        deleteButton.innerText = "Delete"
-        // deleteButton.classList.add("delete-button");
- 
-        todoList.appendChild(deleteButton);
-        todoList.appendChild(newTodo);
+		checkbox.setAttribute("type", "checkbox");
+		newTodo.appendChild(checkbox);
+		newTodo.innerHTML += inputData.value;
+		deleteButton.innerText = "Delete";
+		newTodo.appendChild(deleteButton);
 
-        inputData.value = "";
+		todoList.appendChild(newTodo);
 
-        // delete button
-        // deleteButton.addEventListener("click", () => {
-        //     alert("do you realy want to delete ?");
-        // })
+		deleteButton.addEventListener("click", () => {
+			newTodo.remove();
+		});
 
-        // also add checkbox to your todo while creating.
-        
+		inputData.value = "";
+	});
 
-    })
-    
+	searchInput.addEventListener("keyup", () => {
+		let searchData = searchInput.value;
+		let todos = Array.from(todoList.children);
 
-    searchInput.addEventListener("keyup", () => {
+		for (let i = 0; i < todos.length; i++) {
+			if (todos[i].innerText.includes(searchData)) {
+				todos[i].style.display = "block";
+			} else {
+				todos[i].style.display = "none";
+			}
+		}
+	});
+});
 
-        let searchData = searchInput.value;
+// window.addEventListener("load", () => {
+// 	let inputData = document.getElementById("todo-input");
+// 	let addButton = document.getElementById("todo-add");
+// 	let todoList = document.getElementById("todos-list");
+// 	let searchInput = document.getElementById("todo-search");
 
-        let todos = Array.from(todoList.children);
+// 	addButton.addEventListener("click", () => {
+// 		let newTodo = document.createElement("li");
+// 		let checkbox = document.createElement("input");
+// 		let deleteButton = document.createElement("button");
 
-        // todos.forEach((kubilay) => {
-        //     if(kubilay.innerText.includes(searchData)){
-        //         kubilay.style.display = "block";
-        //     }else{
-        //         kubilay.style.display = "none";
-        //     }
-        // })
+// 		checkbox.setAttribute("type", "checkbox");
+// 		newTodo.appendChild(checkbox);
+// 		newTodo.innerHTML += inputData.value;
+// 		deleteButton.innerText = "Delete";
+// 		newTodo.appendChild(deleteButton);
 
-        for(let i = 0; i < todos.length; i++){
-            if(todos[i].innerText.includes(searchData)){
-                todos[i].style.display = "block";
-            }else{
-                todos[i].style.display = "none";
-            }
-        }
-    })
-})
+// 		todoList.appendChild(newTodo);
+
+// 		deleteButton.addEventListener("click", () => {
+// 			newTodo.remove();
+// 		});
+
+// 		inputData.value = "";
+// 	});
+
+// 	searchInput.addEventListener("keyup", () => {
+// 		let searchData = searchInput.value;
+// 		let todos = Array.from(todoList.children);
+
+// 		for (let i = 0; i < todos.length; i++) {
+// 			if (todos[i].innerText.includes(searchData)) {
+// 				todos[i].style.display = "block";
+// 			} else {
+// 				todos[i].style.display = "none";
+// 			}
+// 		}
+// 	});
+// });
