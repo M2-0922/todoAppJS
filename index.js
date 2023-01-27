f4e230055b71aec16ecc1a86c0f8560a3f55826a
Mayu <mayu.i.19930327@gmail.com>
2023-01-26 16:02:43 -0800

    added showing old list text with confirm when editing save btn is clicked / alert if input value is empty

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ window.addEventListener("load", () => {
 
     // When we edit the todo, after that while we are deleting it shows the old text,
     // please find a way to change this text to new version of text
-
+    // --> ***point1***
+    
     // please put validation for inputs, that you cannot add empty todo to the list.
     // also check while editing.
-
+    // --> ***point2***
+    
     // please search: how we can hold the information in localStorage.
     // you can use stackoverflow, google search, etc.
     // please apply localStorage to todo Application
-
+    
     // once you done with those questions, please create your own branch on top of bug-fix branch
     // branch name ex: bug-fix-kubilay
     // push them to repo
-
+    
     let inputData = document.getElementById("todo-input");
     let addButton = document.getElementById("todo-add");
     let todoList = document.getElementById("todos-list");
     let searchInput = document.getElementById("todo-search");
-
+    
     inputData.addEventListener("keyup", (event) => {
         // console.log(event);
         if(event.key === "Enter"){
@@ -26,88 +28,103 @@ window.addEventListener("load", () => {
             // trigger the event
         }
     });
-
+    
     addButton.addEventListener("click", () => {
         let newTodo = document.createElement("li");
         let deleteButton = document.createElement("button");
         let isCompleted = document.createElement("input");
         let editButton = document.createElement("button");
 
-        // EditButton
-        editButton.innerHTML = "Edit";
-        editButton.classList.add("edit-button");
+        // ***point2***
+        if(inputData.value != "") {
 
-        // Checkbox
-        isCompleted.type = "checkbox";
-        isCompleted.classList.add("todo-checkbox");
+            // EditButton
+            editButton.innerHTML = "Edit";
+            editButton.classList.add("edit-button");
 
-        // Delete button
-        deleteButton.innerHTML = "Delete"
-        deleteButton.classList.add("delete-button");
+            // Checkbox
+            isCompleted.type = "checkbox";
+            isCompleted.classList.add("todo-checkbox");
 
-        // newTodo
-        newTodo.innerHTML = inputData.value;
-        newTodo.appendChild(deleteButton);
-        newTodo.insertBefore(isCompleted, newTodo.childNodes[0]);
-        newTodo.appendChild(editButton);
-        // insertBefore takes 2 arguments, first the element that you want to add
-        // second argument is the location, which node you want to put before
+            // Delete button
+            deleteButton.innerHTML = "Delete"
+            deleteButton.classList.add("delete-button");
 
-        // todoList appends
-        
-        todoList.appendChild(newTodo);
+            // newTodo
+            newTodo.innerHTML = inputData.value;
+            newTodo.appendChild(deleteButton);
+            newTodo.insertBefore(isCompleted, newTodo.childNodes[0]);
+            newTodo.appendChild(editButton);
+            // insertBefore takes 2 arguments, first the element that you want to add
+            // second argument is the location, which node you want to put before
 
-        // reset the input
-        inputData.value = "";
-
-        let valueOfNewTodo = newTodo.innerText.split("Delete")[0];
-
-        deleteButton.addEventListener("click", () => {
-            let isAccept = confirm(valueOfNewTodo + " will deleted, are you sure ?");
-
-            if(isAccept){
-                newTodo.remove();
-                deleteButton.remove();
-            }else{
-                console.log("Sorry");
-            }
-        });
-
-        editButton.addEventListener("click", () => {
-            let editInput = document.createElement("input");
-            let saveButton = document.createElement("button");
-
-            // saveButton
-            saveButton.innerHTML = "Save";
-            saveButton.classList.add("save-button");
-
-            // editInput 
-            editInput.classList.add("edit-input");
-            editInput.value = newTodo.innerText.split("Delete")[0];
-            // ["content", "Edit"]
-
-            newTodo.innerHTML = "";
-            newTodo.appendChild(editInput);
-            newTodo.appendChild(saveButton);
-
-            saveButton.addEventListener("click", () => {
-                newTodo.innerHTML = "";
-                newTodo.appendChild(isCompleted);
-
-                newTodo.appendChild(document.createTextNode(editInput.value));
-                newTodo.appendChild(deleteButton);
-                newTodo.appendChild(editButton);
-                
-            })
+            // todoList appends
             
-            editInput.addEventListener("keyup", (e) => {
-                if(e.key === "Enter"){
-                    saveButton.click();
+            todoList.appendChild(newTodo);
+
+            // reset the input
+            inputData.value = "";
+
+            let valueOfNewTodo = newTodo.innerText.split("Delete")[0];
+
+            deleteButton.addEventListener("click", () => {
+                let isAccept = confirm(valueOfNewTodo + " will deleted, are you sure ?");
+
+                if(isAccept){
+                    newTodo.remove();
+                    deleteButton.remove();
+                }else{
+                    console.log("Sorry");
                 }
+            });
+
+            // editButton event
+            editButton.addEventListener("click", () => {
+                let editInput = document.createElement("input");
+                let saveButton = document.createElement("button");
+
+                // create saveButton
+                saveButton.innerHTML = "Save";
+                saveButton.classList.add("save-button");
+
+                // create editInput
+                editInput.classList.add("edit-input");
+                editInput.value = newTodo.innerText.split("Delete")[0]; // ["content", "Edit"]
+                newTodo.innerHTML = "";
+                newTodo.appendChild(editInput);
+                newTodo.appendChild(saveButton);
+
+                // saveButton event
+                saveButton.addEventListener("click", () => {
+                    // ***point2***
+                    if(editInput.value != "") {
+                        // ***point1***
+                        let isAccept = confirm(valueOfNewTodo + " will change to " + editInput.value + ", are you sure ?");
+                        if(isAccept) {
+                            newTodo.innerHTML = "";
+                            newTodo.appendChild(isCompleted);
+                            newTodo.appendChild(document.createTextNode(editInput.value));
+                            newTodo.appendChild(deleteButton);
+                            newTodo.appendChild(editButton);
+                        } else {
+                            console.log('stay');
+                        }
+                    } else {
+                        alert("Please input something.");
+                    }
+                })
+                
+                editInput.addEventListener("keyup", (e) => {
+                    if(e.key === "Enter"){
+                        saveButton.click();
+                    }
+                })
+
+
             })
-
-
-        })
+        } else {
+            alert("Please input something.");
+        }
     })
 
     searchInput.addEventListener("keyup", () => {
